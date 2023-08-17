ce50f3aec1a84b6814b983e2f3a8c83381668afb
Ibn <omedkane4@gmail.com>
2023-08-17 01:48:20 -0400

    used optimized sectionlist for lazy items and minor visual changes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,81 +1,125 @@
 import FontAwesome from "@expo/vector-icons/FontAwesome"
-import { ImageSourcePropType, Pressable, StatusBar, TextInput, View } from "react-native"
+import { IconButtonProps } from "@expo/vector-icons/build/createIconSet"
+import { ImageSourcePropType, Pressable, SectionList, StatusBar, TextInput, View } from "react-native"
 import { SafeAreaView, Text, Image } from "react-native"
 
+interface QuickChatInfo { name: string, lastMsg: string, unreadNb: number, lastMsgDate: string, avatar: ImageSourcePropType }
+
+const pinnedChats: QuickChatInfo[] = [
+  {
+    avatar: require('../../assets/images/avatars/avatar5.png'),
+    name: "Naruto Uzumaki",
+    lastMsg: "Guess who is the new hokage...",
+    lastMsgDate: "1:21",
+    unreadNb: 1,
+  },
+  {
+    avatar: require('../../assets/images/avatars/avatar2.png'),
+    name: "Madara Uchiha",
+    lastMsg: "Next time my plan will work...",
+    lastMsgDate: "12:52",
+    unreadNb: 4,
+  },
+]
+
+const unpinned: QuickChatInfo[] = [
+  ...[...Array(15).keys()].map(() => (
+    {
+      avatar: require('../../assets/images/avatars/avatar4.png'),
+      name: "Killer Bee",
+      lastMsg: "Yo check out my new rap song...",
+      lastMsgDate: "13:40",
+      unreadNb: 1,
+    }
+  ))
+  ,
+  {
+    avatar: require('../../assets/images/avatars/avatar3.png'),
+    name: "Tenten",
+    lastMsg: "Made a shuriken with bamboo...",
+    lastMsgDate: "Just now",
+    unreadNb: 4,
+  },
+]
+
+const chatSections:
+  {
+    title: string,
+    icon: typeof FontAwesome['Button'] extends React.ComponentClass<IconButtonProps<infer v>> ? v : never,
+    data: QuickChatInfo[]
+  }[] = [
+    {
+      title: "Pinned Chats",
+      icon: "map-pin",
+      data: pinnedChats
+    },
+    {
+      title: "Unpinned Chats",
+      icon: "address-book",
+      data: unpinned,
+    }
+  ]
+
 export default function Layout() {
-  const sbH = StatusBar.currentHeight
   return <SafeAreaView className="flex flex-1 bg-[#1c1c24] py-8">
-    <View style={{ height: sbH }}></View>
+    <View style={{ height: StatusBar.currentHeight }}></View>
 
-    <View className="px-5 flex justify-start items-start">
-      <View className="flex w-full flex-row items-center justify-between">
-        <View className="flex flex-row items-center">
-          <Text className="text-2xl font-gen-semibold text-white">Messages</Text>
-          <Text className="ml-2 text-lg font-gen-medium text-cyan-500">48 new</Text>
+    <SectionList
+      sections={chatSections}
+      ListHeaderComponent={() => (
+        <View className="px-5 mb-5 flex justify-start items-start">
+          <View className="flex w-full flex-row items-center justify-between">
+            <View className="flex flex-row items-center">
+              <Text className="text-2xl font-gen-semibold text-white">Messages</Text>
+              <Text className="ml-2 text-lg font-gen-medium text-cyan-500">
+                <Text className="font-gen-semibold">48</Text> new</Text>
+            </View>
+            <FontAwesome name="edit" color={'#06B6D4'} size={20}></FontAwesome>
+          </View>
+
+          <View className="mt-5 flex flex-row h-14 px-4 bg-black rounded-xl items-center">
+            <TextInput placeholder="Search a chat or a message..." placeholderTextColor={'gray'}
+              className="flex-1 text-white text-base">
+            </TextInput>
+            <FontAwesome name="search" color={'gray'} size={18}></FontAwesome>
+          </View>
+
+          <View className="mt-5 flex flex-row self-center flex-none bg-black rounded-2xl p-1">
+            <View className="h-12 w-20 flex justify-center items-center bg-white rounded-2xl">
+              <Text className="text-black">All</Text>
+            </View>
+            <View className="h-12 w-20 flex justify-center items-center rounded-2xl">
+              <Text className="text-gray-500">Chats</Text>
+            </View>
+            <View className="h-12 w-20 flex justify-center items-center rounded-2xl">
+              <Text className="text-gray-500">Groups</Text>
+            </View>
+          </View>
         </View>
-        <FontAwesome name="edit" color={'#06B6D4'} size={20}></FontAwesome>
-      </View>
-
-      <View className="mt-5 flex flex-row h-14 px-4 bg-black rounded-xl items-center">
-        <TextInput placeholder="Search a chat or a message..." placeholderTextColor={'gray'}
-          className="flex-1 text-white">
-        </TextInput>
-        <FontAwesome name="search" color={'gray'} size={18}></FontAwesome>
-      </View>
-
-      <View className="mt-5 flex flex-row self-center flex-none bg-black rounded-2xl p-1">
-        <View className="h-12 w-20 flex justify-center items-center bg-white rounded-2xl">
-          <Text className="text-black">All</Text>
+      )}
+      SectionSeparatorComponent={() => <View className="mt-4"></View>}
+      renderSectionHeader={(section) => (
+        <View className="flex">
+          <View className="ml-5 flex flex-row items-center">
+            <FontAwesome name={section.section.icon} color={'gray'}></FontAwesome>
+            <Text className="ml-3 text-gray-500 uppercase">{section.section.title}</Text>
+          </View>
         </View>
-        <View className="h-12 w-20 flex justify-center items-center rounded-2xl">
-          <Text className="text-gray-500">Chats</Text>
-        </View>
-        <View className="h-12 w-20 flex justify-center items-center rounded-2xl">
-          <Text className="text-gray-500">Groups</Text>
-        </View>
-      </View>
-    </View>
-
-    <View className="mt-5 flex w-full">
-      <View className="ml-5 flex flex-row items-center">
-        <FontAwesome name="map-pin" color={'gray'}></FontAwesome>
-        <Text className="ml-4 text-gray-500 uppercase">Pinned chats</Text>
-      </View>
-
-      <View className="mt-4">
+      )}
+      renderItem={(s) => (
         <ChatSelector
-          avatar={require('../../assets/images/avatars/avatar5.png')}
-          name="Naruto Uzumaki" lastMsg="Guess who is the new hokage..." lastMsgDate="1:21" unreadNb={1} ></ChatSelector>
-        <ChatSelector
-          avatar={require('../../assets/images/avatars/avatar2.png')}
-          name="Madara Uchiha" lastMsg="Next time my plan will work..." lastMsgDate="12:52" unreadNb={4} ></ChatSelector>
-      </View>
-    </View>
-
-    <View className="mt-4 flex w-full">
-      <View className="flex">
-        <View className="ml-5 flex flex-row items-center">
-          <FontAwesome name="address-book" color={'gray'}></FontAwesome>
-          <Text className="ml-4 text-gray-500 uppercase">All messages</Text>
-        </View>
-      </View>
-      
-      <View className="mt-4">
-        <ChatSelector
-          avatar={require('../../assets/images/avatars/avatar4.png')}
-          name="Killer Bee" lastMsg="Yo check out my new rap song..." lastMsgDate="13:40" unreadNb={1} ></ChatSelector>
-        <ChatSelector
-          avatar={require('../../assets/images/avatars/avatar3.png')}
-          name="Tenten" lastMsg="Made a shuriken with bamboo..." lastMsgDate="Just now" unreadNb={4} ></ChatSelector>
-      </View>
-    </View>
+          avatar={s.item.avatar}
+          name={s.item.name} lastMsg={s.item.lastMsg} lastMsgDate={s.item.lastMsgDate} unreadNb={s.item.unreadNb} ></ChatSelector>
+      )}
+    >
+    </SectionList>
 
   </SafeAreaView>
 }
 
 function ChatSelector(p: { name: string, lastMsg: string, unreadNb: number, lastMsgDate: string, avatar: ImageSourcePropType }) {
   return (
-    <Pressable android_ripple={{color: 'gray'}}>
+    <Pressable android_ripple={{ color: 'gray' }}>
       <View className="flex flex-row w-full py-3 px-5">
         <Image className="h-14 w-14 rounded-full shadow-black" source={p.avatar}></Image>
 
